20e826e0a311018680d9f09791761869a0d4452a
Jatinokov <Enkhtaivan1206@gmail.com>
2020-03-31 15:33:14 -0700

    commented js codes for details

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 
 // computer choose one random letter each time
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+// guessing limit
 var guess = 10;
+// losses start form 0 point
 var losses = 0;
+// win start form 0 point
 var win = 0;
+// empty array that contains clicked letters
 var clickedLetter = [];
-
+// function that randomly choose letters from array
 function randomLetters() {
     var index = Math.floor(Math.random() * letters.length);
 
@@ -15,18 +18,19 @@ function randomLetters() {
     // clickedLetter.push(letters[index])
     return letters[index];
 }
+// invoke the function here also same time assigned to variable
 var computerPicked = randomLetters();
-// creating Array
 
+// onkeypress() method for when click on key-board it will grabs the value of letter
 document.onkeypress = function (event) {
     console.log(event.key);
     console.log(clickedLetter);
-
+// grabbed the value of letter pushed to empty array here
     clickedLetter.push(event.key)
     var myGuess = event.key;
-
+// to DOM the Array which is displays what letters are clicked
     document.getElementById('clickedLetters').innerHTML = clickedLetter;
-
+// conditional statement for matching the letter that computer picked
     if (myGuess === computerPicked) {
         win++;
         guess = 10;
