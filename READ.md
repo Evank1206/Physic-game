2cd98a8ac274342b20d3f8066d568a455feea798
Jatinokov <Enkhtaivan1206@gmail.com>
2020-03-31 15:25:40 -0700

    displyed letters that entered on key-board, using document.getElementById(), and using push() method to clicked letter into empty array

---
+++ READ.md
@@ -0,0 +1,25 @@
+*There are 26 letters on key-board:
+But you you have only 10 times try and to guess the letter for computer automaticlly picked,
+*if you click the number and it matchs for computer picked 
+-the Win score will increments
+*if attemted 10 times and have not matched letter that computer picked
+-the loses score will increments
+Every number you clicked on key-board it will displays
+
+<!-- HARD CODED SAMPLE CODE: -->
+
+<!-- // alert('you have 26 letter guess');
+
+// var answer= 'l';
+// var guess;
+// guess= prompt('What is your guess?');
+
+// for(i=1; i<10; i++){
+//     if(answer == guess){
+//          alert('You guessed correct!');
+//          document.write('Congratulations!!')
+//          break;
+//     }else{
+//             guess = prompt('Please Try Again!'); 
+//     }
+// } -->
--- js/app.js
+++ js/app.js
@@ -1,44 +1,31 @@
-// alert('you have 26 letter guess');
 
-// var answer= 'l';
-// var guess;
-// guess= prompt('What is your guess?');
-
-// for(i=1; i<10; i++){
-//     if(answer == guess){
-//          alert('You guessed correct!');
-//          document.write('Congratulations!!')
-//          break;
-//     }else{
-//             guess = prompt('Please Try Again!'); 
-
-//     }
-
-// }
-
-var letters = [
-"a","b","c", "d", "e",
-"f","g","h", "i", "j", 
-"k","l","m", "n", "o", 
-"p","q","r", "s", "t",
-"u","v","w", "x", "y", "z"  
-];
+// computer choose one random letter each time
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var guess = 10;
 var losses = 0;
 var win = 0;
+var clickedLetter = [];
 
 function randomLetters() {
     var index = Math.floor(Math.random() * letters.length);
-    // console.log(index);
-    console.log (letters[index]);
+
+    console.log(letters[index], index);
+    // this case you can see computer picked letters
+    // clickedLetter.push(letters[index])
     return letters[index];
 }
 var computerPicked = randomLetters();
+// creating Array
 
 document.onkeypress = function (event) {
-    // console.log(event.key);
+    console.log(event.key);
+    console.log(clickedLetter);
+
+    clickedLetter.push(event.key)
     var myGuess = event.key;
+
+    document.getElementById('clickedLetters').innerHTML = clickedLetter;
 
     if (myGuess === computerPicked) {
         win++;
@@ -46,26 +33,23 @@ document.onkeypress = function (event) {
         computerPicked = randomLetters();
         document.getElementById('Guess_left').innerHTML = guess;
         document.getElementById('Wins').innerHTML = win;
-
-    }
-    else {
+        // document.getElementById('clickedLetters').innerHTML = clickedLetter;
+    } else {
         // if guess > 0, then you decrease guess by 1
         // else, you lose, increase losses by 1 and rest guess = 10
         if (guess > 0) {
             guess -= 1;
             document.getElementById('Guess_left').innerHTML = guess;
-        }
-        else {
+        } else {
             losses++;
             guess = 10;
             document.getElementById('Guess_left').innerHTML = guess;
             document.getElementById('loses').innerHTML = losses;
+            // document.getElementById('clickedLetters').innerHTML = clickedLetter;
 
             computerPicked = randomLetters();
         }
     }
-
-
 };
 
 
